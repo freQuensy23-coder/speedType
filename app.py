b6fe4f75147cf4fac17e1627729484ddc498255a
freQuensy23-coder <mametevalex@gmail.com>
2021-08-16 20:32:37 +0300

    Simple console interface

--- app.py
+++ app.py
@@ -1,5 +1,30 @@
 from dict_loader import FileDictLoader as DictLoader
+from random import choice 
+from os import system
+from time import time
+def beep(sound="ok"):
+    pass
 
-words = list(DictLoader("word_rus.txt"))
-print(words)
 
+if __name__ == "__main__":
+    words = list(DictLoader("word_rus.txt"))
+    score = 0
+    symbols = 0
+    game = True 
+    t0 = time()
+    while game:
+        word = choice(words)
+        dt = time() - t0
+        symbols += len(word)
+        print("-"*10)
+        print(word)
+        print("-"*10)
+        print(f"score:   {score}  speed:   {symbols/dt * 60 } s/min")
+        answer = input().lower().strip()
+        if answer == word:
+            beep("ok")
+            score += 1
+        else:
+            beep("wrong")
+            score -= 1
+        system('clear')
