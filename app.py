d884471490b9087433f246a0c923fc5471a7497d
freQuensy23-coder <mametevalex@gmail.com>
2021-08-16 19:29:04 +0300

    Init commit

---
+++ app.py
@@ -0,0 +1,5 @@
+from dict_loader import FileDictLoader as DictLoader
+
+words = list(DictLoader("word_rus.txt"))
+print(words)
+
---
+++ dict_loader.py
@@ -0,0 +1,17 @@
+
+
+class FileDictLoader:
+    def __init__(self, file_name, word_filter=None):
+        self.file = open(file_name, "r")
+        self.filter = word_filter
+
+    def __iter__(self):
+        for word in self.file:
+            if self.filter:
+                if self.filter(word):  
+                    yield word.strip()
+                else:
+                    continue
+
+    def __del__(self):
+        self.file.close()
